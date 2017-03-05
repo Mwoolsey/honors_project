d759858abd9d6ba573c6dd155212dc92d46c1b97
mwoolsey <mwoolsey625@gmail.com>
2017-03-04 20:12:58 -0800

    Started character class

---
+++ character.h
@@ -0,0 +1,16 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <iostream>
+#include <string>
+
+class Character
+{
+ public:
+   Character( std::string name
+ private:
+  std::string _name;
+  int _cur_health, _max_health;
+  int _x_pos, _y_pos;
+};
+#endif /*CHARACTER_H*/
